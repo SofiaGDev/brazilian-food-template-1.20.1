1ddf670e7abbcd770c0816d3e32cc41d99d0a960
Felipepsii <felipe811801@gmail.com>
2024-07-19 17:49:22 -0300

    Restart

--- src/main/java/com/bv/brazilianfoods/BrazilianfoodsDataGenerator.java
+++ src/main/java/com/bv/brazilianfoods/BrazilianfoodsDataGenerator.java
@@ -1,9 +1,9 @@
-package com.bv.brazilianfood;
+package com.bv.brazilianfoods;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class BrazilianFoodDataGenerator implements DataGeneratorEntrypoint {
+public class BrazilianfoodsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
--- src/main/java/com/bv/brazilianfoods/mixin/ExampleMixin.java
+++ src/main/java/com/bv/brazilianfoods/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package com.bv.brazilianfood.mixin;
+package com.bv.brazilianfoods.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;

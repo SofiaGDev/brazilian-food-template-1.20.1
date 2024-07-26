9c38589e326bc4180a7f4ff72f8afa4a37490b52
SofiaGDev <sofia811801@gmail.com>
2024-07-26 19:52:22 -0300

    .

---
+++ src/main/java/com/bv/brazilianfoods/item/custom/MetalDetectorItem.java
@@ -0,0 +1,13 @@
+package com.bv.brazilianfoods.item.custom;
+
+import net.minecraft.item.Item;
+
+public class MetalDetectorItem extends Item {
+
+    public MetalDetectorItem(Settings settings) {
+        super(settings);
+    }
+
+
+    
+}

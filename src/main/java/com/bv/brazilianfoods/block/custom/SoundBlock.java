03e845f664c5317b394a86d132f0373486313b33
SofiaGDev <sofia811801@gmail.com>
2024-08-08 19:27:17 -0300

    Start working in a block

---
+++ src/main/java/com/bv/brazilianfoods/block/custom/SoundBlock.java
@@ -0,0 +1,12 @@
+package com.bv.brazilianfoods.block.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+
+public class SoundBlock extends Block {
+    public SoundBlock(Settings settings) {
+        super(settings);
+    }
+
+
+}
